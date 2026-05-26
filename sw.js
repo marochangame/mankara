21e8c28139d1513caf90906f4482664049adfefe
marochangame <marochan.marosan.dayo@gmail.com>
2026-05-26 12:46:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,2 @@
-const CACHE='mancala-v4-6-chain-ai';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.registration.unregister())));
